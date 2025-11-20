395af9d177b83e38999a70eef55561a7373000fb
Son Daehyeon <sondaehyeon01@gmail.com>
2025-11-20 12:44:54 +0900

    fix(jpa): lazy init 해결

--- src/main/kotlin/com/github/kusitms_bugi/domain/session/infrastructure/jpa/SessionJpaRepository.kt
+++ src/main/kotlin/com/github/kusitms_bugi/domain/session/infrastructure/jpa/SessionJpaRepository.kt
@@ -1,18 +1,23 @@
 package com.github.kusitms_bugi.domain.session.infrastructure.jpa
 
 import com.github.kusitms_bugi.domain.user.infrastructure.jpa.User
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.time.LocalDateTime
 import java.util.*
 
 interface SessionJpaRepository : JpaRepository<Session, UUID> {
 
-    override fun findById(id: UUID): Optional<Session>
+    @Query("SELECT s FROM Session s LEFT JOIN FETCH s.statusHistory LEFT JOIN FETCH s.metrics WHERE s.id = :id")
+    override fun findById(@Param("id") id: UUID): Optional<Session>
 
-    fun findByUser(user: User): List<Session>
+    @Query("SELECT DISTINCT s FROM Session s LEFT JOIN FETCH s.statusHistory LEFT JOIN FETCH s.metrics WHERE s.user = :user")
+    fun findByUser(@Param("user") user: User): List<Session>
 
-    fun findByUserAndCreatedAtAfter(user: User, createdAt: LocalDateTime): List<Session>
+    @Query("SELECT DISTINCT s FROM Session s LEFT JOIN FETCH s.statusHistory LEFT JOIN FETCH s.metrics WHERE s.user = :user AND s.createdAt > :createdAt")
+    fun findByUserAndCreatedAtAfter(@Param("user") user: User, @Param("createdAt") createdAt: LocalDateTime): List<Session>
 
-    fun findByUserAndCreatedAtBetween(user: User, startDate: LocalDateTime, endDate: LocalDateTime): List<Session>
+    @Query("SELECT DISTINCT s FROM Session s LEFT JOIN FETCH s.statusHistory LEFT JOIN FETCH s.metrics WHERE s.user = :user AND s.createdAt BETWEEN :startDate AND :endDate")
+    fun findByUserAndCreatedAtBetween(@Param("user") user: User, @Param("startDate") startDate: LocalDateTime, @Param("endDate") endDate: LocalDateTime): List<Session>
 }
